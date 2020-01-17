ac6cc652d14533f8e87244549c6e8f835931fb84
jmarquez <ing.jdj@gmail.com>
2020-01-17 16:02:28 -0300

    Query..

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, redirect, url_for, g
+import sqlite3
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'holamundo'
 
 
-@app.route('/', methods=['POST', 'GET'], defaults={'name': 'User'})
+def connect_db():
+    sql = sqlite3.connect('C:\Users\jmarquez\PycharmProjects\FlaskCourseUltimate\db\data.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+
+def get_db():
+    if not hasattr(g, 'sqlite3'):
+        g.sqlite_db = connect_db()
+    print(g)
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/', methods=['POST', 'GET'], defaults={'name': 'Usuario'})
 @app.route('/<name>', methods=['POST', 'GET'])
 def index(name):
-    n = name
-    print(n)
-    return '<h1>Hello {} </h1>'.format((n))
+    return render_template('home.html', name=name)
 
 @app.route('/json')
 def jsonh():
@@ -20,16 +38,14 @@ def query():
     print(name)
     return '<h1>Hello {} </h1>'.format(name)
 
-@app.route('/forms')
+@app.route('/forms', methods= ['GET','POST'])
 def forms():
-    form = '''
-        <form method="POST" action="/process">
-            <input type="text" name="name"/>
-            <input type="text" name="location"/>
-             <input type="submit" value="Enviar"/>
-        </form>
-    '''
-    return form
+    if request.method == 'GET':
+        return render_template('forms.html')
+    else:
+        name = request.form['name']
+        location = request.form['location']
+        return redirect(url_for('process'), name=name, location=location)
 
 
 @app.route('/process',  methods=['POST', 'GET'])
@@ -44,6 +60,7 @@ def processjson():
     name = data['name']
     loca = data['location']
     return jsonify({'name': name, 'location': loca})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
